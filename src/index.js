338c5a0fe6ad9d347f61eb0f144972759615da97
umair-khanzada <umairkz52@gmail.com>
2018-05-16 15:41:42 +0500

    create store and add provider as a root component.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+//custom components.
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//css files.
+import './index.css';
+
+const store = createStore(() => {});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
