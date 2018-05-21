3c5c4caead5d4de8f3cee172cee07466c1ad9c51
umair-khanzada <umairkz52@gmail.com>
2018-05-21 17:08:25 +0500

    integrate google login and routing.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {Route} from 'react-router-dom';
 
-import { addEvent, getEvent } from './actions/events';
-import logo from './logo.svg';
+import { getEvent } from './actions/events';
+
+//styles.
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
   }
+
   render() {
-    console.log("in app.js",this.props)
     return (
-       <Router>
-        <div className="App">
-          <nav className="navbar navbar-inverse navbar-fixed-top">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <a className="navbar-brand" href="#">WebSiteName</a>
-              </div>
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-              </ul>
-            </div>
-          </nav>
-          <div className="container bodyContainer">
-              <button type="button" onClick={() => this.props.dispatch(getEvent())}>click me</button>
-          </div>
+      <div className="App">
+        <div className="container bodyContainer">
+          <button type="button" onClick={() => this.props.dispatch(getEvent())}>click me</button>
         </div>
-       </Router>
+        <Route path="/child1" render={() => <h1>child1</h1>} />
+        <Route path="/child2" render={() => <h1>child2</h1>} />
+        <Route path="/child3" render={() => <h1>child3</h1>} />
+        <Route path="/child4" render={() => <h1>child4</h1>} />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware  } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import rootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
@@ -10,13 +11,19 @@ import registerServiceWorker from './registerServiceWorker';
 //custom components.
 import App from './App';
 
-//css files.
+//styles.
 import './index.css';
+import {getEvent} from "./actions/events";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/login" render={() =><h1>Login</h1>} />
+        <Route path="/" render={() => false ? <App /> : <Redirect to="/login" />} />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
