92cb6ed3005ed904325274572641c7e01ad18bcb
umair-khanzada <umairkz52@gmail.com>
2018-05-23 17:27:53 +0500

    add card component in app.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Route, Link} from 'react-router-dom';
 
+//custom component.
+import Card from './components/Card'
+
+//actions.
 import { getEvent } from './actions/events';
 
 //styles.
@@ -18,16 +22,9 @@ class App extends Component {
         <div className="container bodyContainer">
           <button type="button" onClick={() => this.props.dispatch(getEvent())}>click me</button>
         </div>
-        <div>
-          <Link to="/child1">child1</Link><br />
-          <Link to="/child2">child2</Link><br />
-          <Link to="/child3">child3</Link><br />
-          <Link to="/child4">child4</Link>
-        </div>
-        <Route path="/child1" render={() => <h1>child1</h1>} />
-        <Route path="/child2" render={() => <h1>child2</h1>} />
-        <Route path="/child3" render={() => <h1>child3</h1>} />
-        <Route path="/child4" render={() => <h1>child4</h1>} />
+        {
+          [1,2,3,4,5,6,7,8,9,9,9,9].map(() => <Card className="col-sm-4 col-md-3"/>)
+        }
       </div>
     );
   }
