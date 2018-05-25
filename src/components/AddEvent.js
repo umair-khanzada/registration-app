3835f9524b7bdb49e2cf799f93baa2c0ba6b908f
umair-khanzada <umairkz52@gmail.com>
2018-05-25 16:42:14 +0500

    create add event form.

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -17,15 +17,51 @@ class AddEvent extends Component {
 
   render() {
     return (
-        <LocalForm>
-
-        <label htmlFor="name">Name:</label>
-        <Control.text model=".name" />
-
-        <br />
-        <label htmlFor="description">Description:</label>
-        <Control.text model=".description" />
-      </LocalForm>
+      <div className="col-sm-offset-3 col-sm-6">
+        <LocalForm onSubmit={(values) => console.log("values", values)}>
+          <div className="form-group">
+            <label>Name</label>
+            <Control.text model=".name" className="form-control" placeholder="Name of the event" />
+          </div>
+          <div className="form-group">
+            <label>Description</label>
+            <Control.textarea model=".description" className="form-control" placeholder="Describe your event" />
+          </div>
+          <div className="form-group">
+            <label>Tags</label>
+            <Control.text model=".tags" className="form-control" placeholder="Tags" />
+          </div>
+          <div className="form-group">
+            <label>Venue</label>
+            <Control.text model=".venue" className="form-control" placeholder="Place of the event" />
+          </div>
+          <div className="form-group">
+            <label>Start date</label>
+            <Control type="date" model=".startDate" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>End date</label>
+            <Control type="date" model=".endDate" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Organizers</label>
+            {/*TODO: change text input into auto-complete*/}
+            <Control.text model=".organizers" className="form-control" placeholder="Organizers" />
+          </div>
+          <div className="form-group">
+            <label>Trainers</label>
+            {/*TODO: change text input into auto-complete*/}
+            <Control.text model=".trainers" className="form-control"  placeholder="Trainers" />
+          </div>
+          <div className="form-group">
+            <label>Invites</label>
+            <Control.text model=".invites" className="form-control" placeholder="invite friends"/>
+          </div>
+          <div>
+            <button type="submit" className="btn btn-default pull-right">Submit</button>
+          </div>
+        </LocalForm>
+      </div>
     );
   }
 }
