9f4cb7ad452f76a405a1258d8c2a61f1bd640339
umair-khanzada <umairkz52@gmail.com>
2018-05-29 17:08:00 +0500

    implement logout.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,37 @@ import Avatar from './Avatar';
 
 const user = JSON.parse(localStorage.getItem('user'));
 
+//define globally for ignore esLint warning.
+/* global gapi */
+
 class Navbar extends Component {
+  constructor(props){
+    super(props);
+
+    this.signOut = this.signOut.bind(this);
+  }
+
+  componentDidMount(){
+    //timeout due to gapi loaded after did mount,
+    //will be remove ones move auth on server side.
+    setTimeout(() => {
+      gapi.load('auth2', function() {
+        gapi.auth2.init();
+      });
+    }, 5000);
+  }
+
+  signOut(){
+    // TODO: temporary, will update when move auth on server.
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut()
+      .then(() => {
+        console.log('User signed out.');
+        localStorage.removeItem('user');
+        window.location.href = 'http://localhost:3000';
+      });
+  }
+
   render(){
     return (
       <nav className="navbar navbar-default">
@@ -31,6 +61,9 @@ class Navbar extends Component {
               <ul className="nav navbar-nav navbar-right">
                 <li>
                   <Avatar title={user.name} image={user.image} />
+                </li>
+                <li>
+                  <a href="javascript:" onClick={this.signOut}>Sign out</a>
                 </li>
               </ul>
             </div>
