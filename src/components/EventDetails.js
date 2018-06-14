a8450204f89c9b30351110e143320971da9db928
umair-khanzada <umairkz52@gmail.com>
2018-06-14 10:31:09 +0500

    card design improvement .

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -10,8 +10,8 @@ class EventDetails extends Component {
     const { events } = this.props;
     return (
       <div>
-        {/*TODO: temporary condition*/}
-        {events.length && <Card event={events[0]} className="col-sm-12"/>}
+        {/*TODO: temporary condition, also check if not exists in redux store get event from DB.*/}
+        {events.length && <Card event={events[0]} className="col-sm-9" isDetail/>}
         {/*<NotFound name="Events" visible={true}/>*/}
       </div>
     );
