b43e36558c4c02a1fda5b2c250f8947784fbc806
umair-khanzada <umairkz52@gmail.com>
2018-06-14 10:29:38 +0500

    move all constant in single export.

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,9 +1,23 @@
 //Form error configuration.
-export const REQUIRED = (value) => !!value;
-export const ARRAY_LENGTH = (value) => value && value.length;
-export const MIN_LENGTH = (value) => value && value.length >= 100;
+const REQUIRED = (value) => !!value;
+const ARRAY_LENGTH = (value) => value && value.length;
+const MIN_LENGTH = (value) => value && value.length >= 100;
 
-export const REQUIRED_MESSAGE = { REQUIRED: 'This field is required.' };
-export const MIN_LENGTH_MESSAGE = { MIN_LENGTH: 'Put at least 100 character in this field.' };
+const REQUIRED_MESSAGE = { REQUIRED: 'This field is required.' };
+const MIN_LENGTH_MESSAGE = { MIN_LENGTH: 'Put at least 100 character in this field.' };
+const TOUCH_AND_NOT_FOCUS = {touched: true, focus: false};
 
-export const TOUCH_AND_NOT_FOCUS = {touched: true, focus: false};
+
+//Helper functions.
+const GET_DETAIL = (src = [], filterBy, key) => src.filter(obj => filterBy.includes(obj[key]));
+
+
+export {
+  REQUIRED,
+  ARRAY_LENGTH,
+  MIN_LENGTH,
+  REQUIRED_MESSAGE,
+  MIN_LENGTH_MESSAGE,
+  TOUCH_AND_NOT_FOCUS,
+  GET_DETAIL
+}
