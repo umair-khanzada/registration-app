759f11a0a64b09f4341b114f8672b8fe285d8739
umair-khanzada <umairkz52@gmail.com>
2018-06-27 18:14:31 +0500

    functional event details component.

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -7,11 +7,12 @@ import NotFound from './NotFound';
 
 class EventDetails extends Component {
   render() {
-    const { events } = this.props;
+    const { events, match: {params: {id}} } = this.props;
+    const event = events.find(e => e._id.$oid == id);
     return (
       <div>
         {/*TODO: temporary condition, also check if not exists in redux store get event from DB.*/}
-        {events.length && <Card event={events[0]} className="col-sm-9" isDetail/>}
+        {events.length && <Card event={event} className="col-sm-9" isDetail/>}
         {/*<NotFound name="Events" visible={true}/>*/}
       </div>
     );
