1620b1ccba6e96917421697e69a8b5c1b690d2cf
umair-khanzada <umairkz52@gmail.com>
2018-06-27 18:16:06 +0500

    pass handleClick on card from events component.

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import history from '../history';
 
 //custom component.
 import Card from './Card';
@@ -7,16 +8,22 @@ import Spinner from './Spinner';
 import NotFound from './NotFound';
 
 class Events extends Component {
+
+  handleCardClick(id){
+    history.push(`/event/${id}`);
+  }
+
   render() {
     const { events, loading } = this.props;
     return (
       <div>
         <Spinner visible={loading} size={30}/>
-        { events.map((event, i) => <Card event={event} key={i} />) }
+        { events.map((event, i) => <Card event={event} key={i} handleClick={this.handleCardClick}/>) }
         <NotFound name="Events" visible={!events.length && !loading}/>
       </div>
     );
   }
+  
 }
 
 export default connect((state) => {
