5659a65e7363775325d4f61a1a9ae71500770570
umair-khanzada <umairkz52@gmail.com>
2018-06-28 22:04:38 +0500

    change min length of description.

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,10 +1,10 @@
 //Form error configuration.
 const REQUIRED = (value) => !!value;
 const ARRAY_LENGTH = (value) => value && value.length;
-const MIN_LENGTH = (value) => value && value.length >= 100;
+const MIN_LENGTH = (value) => value && value.length >= 10;
 
 const REQUIRED_MESSAGE = { REQUIRED: 'This field is required.' };
-const MIN_LENGTH_MESSAGE = { MIN_LENGTH: 'Put at least 100 character in this field.' };
+const MIN_LENGTH_MESSAGE = { MIN_LENGTH: 'Put at least 10 character in this field.' };
 const TOUCH_AND_NOT_FOCUS = {touched: true, focus: false};
 
 
