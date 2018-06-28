8ba663604a3fd5411385ef6a937c1062e89db750
umair-khanzada <umairkz52@gmail.com>
2018-06-28 22:03:20 +0500

    update avatarGroup component.

--- src/components/AvatarGroup.js
+++ src/components/AvatarGroup.js
@@ -9,21 +9,16 @@ import Avatar from './Avatar';
 import { GET_DETAIL } from '../constants/global';
 
 const AvatarGroup = ({users, members, size}) => {
-  console.log("users in  ag", users)
+  const avatarUsers = users.filter(user => members.includes(user._id.$oid));
   return (
     <div className="avatar-group">
-      {members.map(({name, image}, i) => <span style={{position: 'absolute', left: (i*18)+'px'}}><Avatar key={i} title={name} image={image} size={size}/></span>)}
+      {avatarUsers.map(({name, image}, i) => <span key={i} style={{position: 'absolute', left: (i*18)+'px'}}><Avatar title={name} image={image} size={size}/></span>)}
     </div>
   )
 };
 
 AvatarGroup.propTypes = {
-  members: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      image: PropTypes.string
-    })
-  ).isRequired,
+  members: PropTypes.arrayOf(PropTypes.string).isRequired,
   size: PropTypes.oneOf(['sm', 'md'])
 };
 
