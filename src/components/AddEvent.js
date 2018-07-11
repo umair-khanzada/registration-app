76d472c9f02eda8c9ed1e0f18181eb27089fa4d2
umair-khanzada <umairkz52@gmail.com>
2018-07-11 18:51:33 +0500

    add time feature.

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import TimePicker from 'react-times';
+import moment from 'moment';
 import Tags from './Tags';
+
+import 'react-times/css/material/default.css';
+import 'react-times/css/classic/default.css';
 
 // constants.
 import { 
@@ -27,30 +32,46 @@ const OptionList = ({data, label, value, nested}) => {
 class AddEvent extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      formSubmit: false
-    };
 
+    let time = `${moment().hours()}:${moment().minutes()}`;
     this.initialEvent = {
-      startDate: new Date(),
-      endDate: new Date(),
+      timings: [{date: new Date(), start: time, end: time}],
+      // endDate: new Date(),
       tags: [],
       organizers: [],
       trainers: [],
       invites: [],
       attendees: [],
       externalInvites: []
-    }
+    };
+
+    this.state = {
+      start: {time, meridiem: 'AM', focused: false},
+      end: {time, meridiem: 'AM', focused: false}
+    };
+  }
+
+  onTimeChange({hour, minute, meridiem, name}) {
+    this.setState({[name]: {time: `${hour}:${minute}`, meridiem, focused: true}})
   }
 
   createEvent(data) {
     console.log("data", data);
-    this.setState({ formSubmit: true });
-    this.props.dispatch(createEvent(data));
+    /* TODO: convert date into array of date and time object, eg: [{date: '27/07/2018', time: '10:10 am'}]*/
+    const {start, end} = this.state,
+      time = {
+        startTime: `${start.time} ${start.meridiem}`,
+        endTime: `${end.time} ${end.meridiem}`
+      },
+      payload = Object.assign({}, data, time);
+    console.log("payload", payload)
+    this.props.dispatch(createEvent(payload));
   }
 
   render() {
-    const {users} = this.props;
+    const {users} = this.props,
+      {time: startTime, meridiem: startMeridiem, focused: startFocused } = this.state.start,
+      {time: endTime, meridiem: endMeridiem, focused: endFocused } = this.state.end;
     return (
       <div className="col-sm-offset-3 col-sm-6">
         <LocalForm initialState={this.initialEvent} onSubmit={(values) => this.createEvent(values)}>
@@ -92,13 +113,26 @@ class AddEvent extends Component {
               messages={REQUIRED_MESSAGE}
             />
           </div>
+          {/*
+            TODO: Add button for repeat date and time block,
+            TODO: Fix picker open issue,
+            TODO: Apply error handling on time eg: (end time must be grater)
+          */}
           <div className="form-group">
-            <label>Start date</label>
+            <label>Date</label>
             <Control type="date" model=".startDate" className="form-control" />
           </div>
-          <div className="form-group">
-            <label>End date</label>
-            <Control type="date" model=".endDate" className="form-control" />
+          <div className="form-group inline-element m-r-10">
+            <label>Start time</label>
+            <TimePicker timeMode="12" key="start"
+              time={startTime} focused={startFocused} meridiem={startMeridiem}
+              onTimeChange={(time) => this.onTimeChange({...time, name: 'start'})} />
+          </div>
+          <div className="form-group inline-element m-l-10">
+            <label>End time</label>
+            <TimePicker timeMode="12" key="end"
+              time={endTime} focused={endFocused} meridiem={endMeridiem}
+              onTimeChange={(time) => this.onTimeChange({...time, name: 'end'})} />
           </div>
           <div className="form-group">
             <label>Organizers</label>
